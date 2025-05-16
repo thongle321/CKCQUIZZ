f2883202a7a18e020e4272ea547a2a5d4e4baf3d
thongle321 <thongle857@gmail.com>
2025-05-16 20:42:59 +0700

    Change landingpage and login with jwt

--- CKCQUIZZ.Server/obj/Debug/net9.0/CKCQUIZZ.Server.AssemblyInfo.cs
+++ CKCQUIZZ.Server/obj/Debug/net9.0/CKCQUIZZ.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CKCQUIZZ.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27127041d7c6552c5d99bf7bd52d40f4775e89d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16218954c0694280eeaeb3200be9bedf7727171e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CKCQUIZZ.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CKCQUIZZ.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
