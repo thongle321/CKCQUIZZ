ddd0d647ba1059ccd2f3993bd1ce9d6b00c8772d
dxhoangsteve <sterbe2k4@gmail.com>
2025-06-04 14:29:06 +0700

    Delete UI

--- ckcandr/lib/main.dart
+++ ckcandr/lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:ckcandr/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: App(),
+      child: MyApp(),
     ),
   );
 }
