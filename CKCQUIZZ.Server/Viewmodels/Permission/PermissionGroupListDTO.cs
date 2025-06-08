3e8586842ffe4a8b18a6099a110c9e33354484e4
thongle321 <thongle857@gmail.com>
2025-06-08 15:08:38 +0700

    fix lỗi warning

--- CKCQUIZZ.Server/Viewmodels/Permission/PermissionGroupListDTO.cs
+++ CKCQUIZZ.Server/Viewmodels/Permission/PermissionGroupListDTO.cs
@@ -2,8 +2,8 @@ namespace CKCQUIZZ.Server.Viewmodels.Permission
 {
     public class PermissionGroupListDTO
     {
-    public string Id { get; set; } 
-    public string TenNhomQuyen { get; set; }
+    public string Id { get; set; } = default!;
+    public string TenNhomQuyen { get; set; } = default!;
     public int SoNguoiDung { get; set; }
     }
 }
